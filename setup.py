37c984c4193b46be75cd83db2971af788ac03e97
Sukhsimran Singh <sukhsimransingh304@gmail.com>
2025-12-30 11:42:18 +0530

    setup.py

--- setup.py
+++ setup.py
@@ -14,6 +14,7 @@ def get_requirements(file_path:str)->List[str]:
         if HYPHEN_E_DOT in requirements:
             requirements.remove(HYPHEN_E_DOT)
 
+
 setup(
 name='End to End ML Project',
 version='0.0.1',
